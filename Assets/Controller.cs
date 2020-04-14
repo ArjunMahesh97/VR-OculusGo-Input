c3ecc9809d5f867723a0159e23674e2a407d0c74
unknown <arjunmahesh97@gmail.com>
2020-04-14 13:31:47 +0530

    Pointer Controller

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    GameObject gObject;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gObject = new GameObject();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider != null)
+            {
+                if (gObject != hit.collider.gameObject)
+                {
+                    gObject.transform.SendMessage("OnVRExit");
+                    gObject = hit.transform.gameObject;
+                    gObject.transform.SendMessage("OnVREnter");
+                }
+            }
+        }
+        else
+        {
+            if (gObject != null) ;
+            {
+                gObject.transform.SendMessage("OnVRExit");
+                gObject = null;
+            }
+        }
     }
 }
---
+++ Assets/ToolTip.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class ToolTip : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI helpText;
+    [SerializeField] TextMeshProUGUI toolTipText;
+
+    [SerializeField] string helpString;
+    [SerializeField] string tipString;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        helpText = GameObject.Find("HelpText").GetComponent<TextMeshProUGUI>();
+        toolTipText = GameObject.Find("ToolTipText").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnVREnter()
+    {
+        helpText.text = helpString;
+        toolTipText.text = tipString;
+    }
+    
+    void OnVRExit()
+    {
+        helpText.text = "";
+        toolTipText.text = "";
+    }
+
+}
